1b583ad2343f5663bc90d3fdf160a92b04c2af01
Suzuki Kaito <e215727@ie.u-ryukyu.ac.jp>
2021-12-20 11:08:01 +0900

    1st Step

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215727/Report5.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e215727;
+
+public class Report5 {
+    public static void main(String[] args){
+        String str = null;
+        int num;
+        num = str.length();
+        System.out.println(str + "は" + num + "文字");
+    }
+}
+
+
