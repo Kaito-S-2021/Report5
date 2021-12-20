ac03b177599545adeafd01f6d3c0808d20f74a32
Suzuki Kaito <e215727@ie.u-ryukyu.ac.jp>
2021-12-20 11:51:18 +0900

    2nd Step

--- app/src/main/java/jp/ac/uryukyu/ie/e215727/Report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215727/Report5.java
@@ -3,10 +3,11 @@ package jp.ac.uryukyu.ie.e215727;
 public class Report5 {
     public static void main(String[] args){
         String str = null;
-        int num;
-        num = str.length();
-        System.out.println(str + "は" + num + "文字");
+        try{
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionというエラーが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
-
-
